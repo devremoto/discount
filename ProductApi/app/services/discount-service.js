7facbd3cdc52086f6dd9c48448f35376cb7e0ce3
Adilson Pedro <a.pedro@AVIVATEC.COM.BR>
2019-12-09 15:51:04 -0300

    add circuit breaker

--- ProductApi/app/services/discount-service.js
+++ ProductApi/app/services/discount-service.js
@@ -1,10 +1,28 @@
 const client = require('../infra/grpc/client/discount.client');
+const { createRpcCommand } = require("../infra/circuitbreaker/command-factory");
+// Let's say rpcClient.get(request, (err, res) ...) is your function
+// You really need .bind()!
+
+const request = new messages.YourMessage();
+
+command
+  .execute(request)
+  .then(response => {
+    // do something with response
+  })
+  .catch(error => {
+    // do something with error
+  });
 
 const discountService = {
   discount: userId => {
     return new Promise((resolve, reject) => {
-      const discountRPCService = client.getService('Discount');
 
+
+
+      const discountRPCService = client.getService('Discount');
+      //const command = createRpcCommand(discountRPCService.GetDiscount.bind(discountRPCService));
+      //discountRPCService.GetDiscount({ userId })
       discountRPCService.GetDiscount({ userId }, (error, response) => {
         error ? reject(error) : resolve(response);
       });
